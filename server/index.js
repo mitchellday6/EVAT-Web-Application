cc6599671a44831422839894fd8a256aeeaed740
ShubhUniyal <s223531994@deakin.edu.au>
2024-09-17 04:07:46 +1000

    Added the mongoDB class for connecting with mongoDB cloud

--- server/index.js
+++ server/index.js
@@ -10,6 +10,19 @@ const VehicleRoutes = require("./routes/VehicleRoutes");
 const UserRoutes = require("./routes/UserRoutes");
 const ConfigRoutes = require("./routes/ConfigRoutes");
 
+//mongodb connection
+const MongoDB = require('./db/mongoDB');
+
+const mongoDB = new MongoDB();
+
+// Connect to MongoDB when the server starts
+mongoDB.connect()
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit if the database connection fails
+    });
+
 //global vars
 const PORT = process.env.PORT || 3000;
 
@@ -29,6 +42,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
+process.on('SIGINT', async () => {
+    await mongoDB.close();
+    process.exit(0);
+});
 
 app.listen(PORT, () => {
     console.log("Server Listening on port: " + PORT);
